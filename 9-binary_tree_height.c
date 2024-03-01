ef9445b7065eed981b8789beab0e4ecb01a93e9a
aimane yousr <aimaneyousr@gmail.com>
2024-03-01 19:09:25 +0100

    Fix calculation of binary tree height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,18 +9,22 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t left_height = 0, right_height = 0;
 
 	/* If tree is NULL, return 0 */
 	if (tree == NULL)
 		return (0);
 
 	/* Compute the height of the left subtree */
-	left_height = binary_tree_height(tree->left);
+	if (tree->left != NULL)
+		left_height = 1 + binary_tree_height(tree->left);
 
 	/* Compute the height of the right subtree */
-	right_height = binary_tree_height(tree->right);
+	if (tree->right != NULL)
+		right_height = 1 + binary_tree_height(tree->right);
 
-	/* Return the maximum of left & right subtree heights + 1 for current node */
-	return (left_height > right_height ? left_height + 1 : right_height + 1);
+	if (tree->left > tree->right)
+		return (left_height);
+	else
+		return (right_height);
 }
