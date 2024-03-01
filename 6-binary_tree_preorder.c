3a392cd4af50627e292939e7a25c98e2143c9547
aimane yousr <aimaneyousr@gmail.com>
2024-03-01 16:53:22 +0100

    Refactor binary_tree_preorder function

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -7,14 +7,14 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-  if (tree == NULL  || func == NULL)
-    return;
+	if (tree == NULL  || func == NULL)
+		return;
 
-  /* Call the function to process the current node */
-  func(tree->n);
+	/* Call the function to process the current node */
+	func(tree->n);
 
-  /* Traverse the left subtree */
-  binary_tree_preorder(tree->left, func);
-  /* Traverse the right subtree */
-  binary_tree_preorder(tree->right, func);
+	/* Traverse the left subtree */
+	binary_tree_preorder(tree->left, func);
+	/* Traverse the right subtree */
+	binary_tree_preorder(tree->right, func);
 }
